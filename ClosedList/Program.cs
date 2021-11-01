40add6ed3f6b9ca11ae0c20ba981a0e1f453e1f1
wefall <doginthespace@gmail.com>
2021-11-01 21:05:35 +0300

    clear

--- ClosedList/Program.cs
+++ ClosedList/Program.cs
@@ -7,22 +7,6 @@ namespace ClosedList
     {
         static void Main(string[] args)
         {
-            CircleClosedList<string> s = new CircleClosedList<string>();
-            s.Add("b");
-            s.Add("c");
-
-            s.Add("d");
-            s.Clear();
-            //s.Clear();
-            // u = s[0];
-            // u1 = s[1];
-
-            var t = s.Contains("c");
-        }
-
-        static private void DoSomething(object sender, string e)
-        {
-            Console.WriteLine("qq");
         }
     }
 }
